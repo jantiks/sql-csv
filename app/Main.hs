ec83d14c4fa99591701b5e22fc9ff44b8b3543f8
jantiks <tigran.arsenyan.2018@gmail.com>
2024-06-30 18:18:49 +0200

    added error handling, added '>=' and '<=' operators

--- app/Main.hs
+++ app/Main.hs
@@ -9,14 +9,19 @@ import System.Environment (getArgs)
 import Data.List (intercalate)
 import qualified Data.Text as T
 import Debug.Trace (trace)
+import System.Exit (exitFailure)
 
 main :: IO ()
 main = do
   args <- getArgs
-  let input = intercalate " " args
-  case parse parseSQL "" input of
-    Left err  -> print err
-    Right sql -> executeSQL sql
+  if null args
+     then do 
+        showUsage
+        exitFailure
+  else
+    case parse parseSQL "" (intercalate " " args) of
+        Left err  -> print err
+        Right sql -> executeSQL sql
 
 executeSQL :: SQLQuery -> IO ()
 executeSQL (SelectQuery fields table whereClause) = do
@@ -31,12 +36,16 @@ executeSQL (SelectQuery fields table whereClause) = do
 executeSQL (DeleteQuery table whereClause) = do
     case whereClause of
         Nothing -> putStrLn "DELETE Query requires a WHERE clause"
-        Just (Condition f op v) -> CF.runDeleteQuery table (CF.Condition (T.pack f) (T.pack op) (T.pack v))
+        Just (Condition f op v) -> 
+            CF.runDeleteQuery table (CF.Condition (T.pack f) (T.pack op) (T.pack v))
 
 executeSQL (UpdateQuery table updates whereClause) = do
     case whereClause of
         Nothing -> putStrLn "UPDATE Query requires a WHERE clause"
-        Just (Condition f op v) -> CF.runUpdateQuery table (map (\(fld, val) -> (T.pack fld, T.pack val)) updates) (CF.Condition (T.pack f) (T.pack op) (T.pack v))
+        Just (Condition f op v) -> 
+            CF.runUpdateQuery table 
+            (map (\(fld, val) -> (T.pack fld, T.pack val)) updates) 
+            (CF.Condition (T.pack f) (T.pack op) (T.pack v))
         
 executeSQL (InsertQuery table fields values) = do
     putStrLn $ "asd fileName: " ++ table
@@ -44,3 +53,8 @@ executeSQL (InsertQuery table fields values) = do
     putStrLn $ "values: " ++ show values
 
     CF.runInsertQuery table (map T.pack fields) (map T.pack values)
+
+
+showUsage :: IO ()
+showUsage = do 
+    putStrLn "Usage: sql-csv-exe '<Command>' '<Filepath>' WHERE '<Condition>'"
